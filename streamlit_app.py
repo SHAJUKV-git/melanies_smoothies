e86a5d580e593e9590d04b0625b9f4c8e51e54a7
SHAJUKV-git <shajukv@hotmail.com>
2026-06-30 21:57:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 #import os
+import requests  
 
 # Write directly to the app
 st.title(f"Customoize Your Smoothie :cup_with_straw: {st.__version__}")
@@ -32,13 +33,16 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect('Choose upto 5 ingredientss:', my_dataframe, max_selections=5)
 
 if ingredients_list:
-   st.write(ingredients_list)   # flattened into rows
-   st.text(ingredients_list)    # array list
+   # st.write(ingredients_list)   # to view as flattened into rows
+   # st.text(ingredients_list)    # to view in array list form
 
    ingredients_string = ''
 
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
+       st.subheader(fruit_chosen + ' Nutrition Information')
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
    st.write(ingredients_string)
    
@@ -54,9 +58,3 @@ if ingredients_list:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!,' + name_on_order +'!', icon="✅")
 
-# New section to display smoothiefroot nutrition information #########################################
-import requests  
-# smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())        # instead put the JSON into dataframe as per below statement ################## 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
